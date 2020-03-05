0553e5f53f2460a9326321693814be04796fb4e4
Daniel <qiuzhanghua@icloud.com>
2020-03-05 08:07:14 +0800

    query data

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,26 @@ fn main() -> std::result::Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+
+/*
+create table people
+(
+	id bigserial
+		constraint people_pk
+			primary key,
+	name varchar(80) not null,
+	email varchar(128) not null,
+	enabled bool default false
+);
+*/
+#[derive(Debug, PartialEq, Eq)]
+pub struct Person {
+    id: i64,
+    name: String,
+    email: String,
+    enabled: Option<bool>,
+}
+
 pub fn query_databases(
     conn: &mut PooledConnection<PostgresConnectionManager<NoTls>>,
 ) -> std::result::Result<Vec<String>, Box<dyn Error>> {
@@ -47,10 +67,10 @@ pub fn query_columns(
     conn: &mut PooledConnection<PostgresConnectionManager<NoTls>>,
     table_name: &str,
 ) -> std::result::Result<Vec<(String, String, Option<i32>, bool)>, Box<dyn Error>> {
-    if table_name.contains(' ') {
-        // 小心SQL注入问题
-        return Err(Box::<dyn Error>::from("table name error")); // sample for sql injection
-    };
+    // if table_name.contains(' ') {
+    //     // 小心SQL注入问题
+    //     return Err(Box::<dyn Error>::from("table name error")); // sample for sql injection
+    // };
     let qr = conn.query(
         r##"SELECT column_name, data_type, character_maximum_length, is_nullable, column_default
         FROM information_schema.columns WHERE table_schema = 'public' and table_name = $1;"##,
@@ -81,9 +101,37 @@ pub fn query_columns(
     Ok(cols)
 }
 
+pub fn query_data(
+    conn: &mut PooledConnection<PostgresConnectionManager<NoTls>>,
+    sql: &str,
+    name: &str,
+    enabled: bool,
+    limit: i64,
+    offset: i64,
+) -> std::result::Result<Vec<Person>, Box<dyn Error>> {
+    let qr = conn.query(sql, &[&name, &enabled, &limit, &offset])?;
+
+    let people = qr
+        .iter()
+        .map(|row| {
+            let id: i64 = row.get(0);
+            let name: &str = row.get(1);
+            let email: &str = row.get(2);
+            let enabled: Option<bool> = row.get(3);
+            Person {
+                id,
+                name: name.to_string(),
+                email: email.to_string(),
+                enabled,
+            }
+        })
+        .collect();
+    Ok(people)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{query_columns, query_databases, query_tables};
+    use crate::{query_columns, query_data, query_databases, query_tables};
     use postgres::{Config, NoTls};
     use r2d2_postgres::PostgresConnectionManager;
     use std::str::FromStr;
@@ -127,11 +175,26 @@ mod tests {
         let manager = PostgresConnectionManager::new(config, NoTls);
         let pool = Arc::new(r2d2::Pool::builder().max_size(4).build(manager).unwrap());
         let mut conn = pool.get().unwrap();
-        let table_names = query_columns(&mut conn, "users");
-        if let Ok(x) = table_names {
+        let column_names = query_columns(&mut conn, "users");
+        if let Ok(x) = column_names {
             println!("{:?}", x);
         } else {
-            assert_eq!(false, table_names.is_err())
+            assert_eq!(false, column_names.is_err())
+        }
+    }
+
+    #[test]
+    fn test_query_2() {
+        dotenv::dotenv().ok();
+        let db_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let config = Config::from_str(&db_url).unwrap();
+        let manager = PostgresConnectionManager::new(config, NoTls);
+        let pool = Arc::new(r2d2::Pool::builder().max_size(4).build(manager).unwrap());
+        let mut conn = pool.get().unwrap();
+        if let Ok(people) = query_data(&mut conn, "select * from people where name=$1 and enabled=$2 limit $3 offset $4", "Daniel", true, 10, 0) {
+            println!("{:?}", people)
+        } else {
+            assert!(false, "Query err")
         }
     }
 }
